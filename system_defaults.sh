bffb0a992a99738f90eb882b6701f2f2447acb1c
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2018-04-08 12:41:30 +1000

    tidy up system_defaults.sh

--- system_defaults.sh
+++ system_defaults.sh
@@ -44,7 +44,7 @@ sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.SubmitDiagInfo.
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # SSD-specific tweaks 
-running "Disable local Time Machine snapshots"
+# Disable local Time Machine snapshots
 sudo tmutil disablelocal;ok
 
 COMPUTER_NAME="dylan"
@@ -55,24 +55,24 @@ sudo scutil --set HostName "$COMPUTER_NAME"
 sudo scutil --set LocalHostName "$COMPUTER_NAME"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
 
-running "Stop iTunes from responding to the keyboard media keys"
+# Stop iTunes from responding to the keyboard media keys"
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null;ok
 
-running "Show icons for hard drives, servers, and removable media on the desktop"
+# Show icons for hard drives, servers, and removable media on the desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true;ok
 
-running "Wipe all (default) app icons from the Dock"
+# Wipe all (default) app icons from the Dock"
 # This is only really useful when setting up a new Mac, or if you don’t use
 # the Dock to launch apps.
 defaults write com.apple.dock persistent-apps -array "";ok
 
-running "Disable the sound effects on boot"
+# Disable the sound effects on boot"
 sudo nvram SystemAudioVolume=" ";ok
 
-running "Menu bar: hide the Time Machine, Volume, User, and Bluetooth icons"
+# Menu bar: hide the Time Machine, Volume, User, and Bluetooth icons"
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
   defaults write "${domain}" dontAutoLoad -array \
     "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
@@ -86,16 +86,16 @@ defaults write com.apple.systemuiserver menuExtras -array \
   "/System/Library/CoreServices/Menu Extras/Clock.menu"
 ok
 
-running "Save to disk (not to iCloud) by default"
+# Save to disk (not to iCloud) by default"
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false;ok
 
-running "Disable the “Are you sure you want to open this application?” dialog"
+# Disable the “Are you sure you want to open this application?” dialog"
 defaults write com.apple.LaunchServices LSQuarantine -bool false;ok
 
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
-running "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
 defaults write com.apple.screencapture type -string "png";ok
 
 # Disable screenshot shadow
@@ -113,52 +113,52 @@ defaults write com.apple.finder ShowStatusBar -bool true
 # Avoid creating .DS_Store files on network volumes
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-running "Show hidden files by default"
+# Show hidden files by default"
 defaults write com.apple.finder AppleShowAllFiles -bool true;ok
 
-running "Show all filename extensions"
+# Show all filename extensions"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true;ok
 
-running "Allow text selection in Quick Look"
+# Allow text selection in Quick Look"
 defaults write com.apple.finder QLEnableTextSelection -bool true;ok
 
-running "When performing a search, search the current folder by default"
+# When performing a search, search the current folder by default"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf";ok
 
-running "Disable the warning when changing a file extension"
+# Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false;ok
 
-running "Automatically open a new Finder window when a volume is mounted"
+# Automatically open a new Finder window when a volume is mounted"
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true;ok
 
-running "Disable the warning before emptying the Trash"
+# Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false;ok
 
-running "Empty Trash securely by default"
+# Empty Trash securely by default"
 defaults write com.apple.finder EmptyTrashSecurely -bool true;ok
 
-running "Show the ~/Library folder"
+# Show the ~/Library folder"
 chflags nohidden ~/Library;ok
 
-running "Minimize windows into their application’s icon"
+# Minimize windows into their application’s icon"
 defaults write com.apple.dock minimize-to-application -bool true;ok
 
-running "Disable Dashboard"
+# Disable Dashboard"
 defaults write com.apple.dashboard mcx-disabled -bool true;ok
 
-running "Don’t show Dashboard as a Space"
+# Don’t show Dashboard as a Space"
 defaults write com.apple.dock dashboard-in-overlay -bool true;ok
 
-running "Disable Spotlight indexing for any volume that gets mounted and has not yet been indexed"
+# Disable Spotlight indexing for any volume that gets mounted and has not yet been indexed"
 # Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
 sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes";ok
 
-running "Prevent Time Machine from prompting to use new hard drives as backup volume"
+# Prevent Time Machine from prompting to use new hard drives as backup volume"
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true;ok
 
-running "Disable local Time Machine backups"
+# Disable local Time Machine backups"
 hash tmutil &> /dev/null && sudo tmutil disablelocal;ok
 
 # Show all processes in Activity Monitor
