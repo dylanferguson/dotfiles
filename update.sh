eeda7f0d182f9cc2e1ad5258b9ee87e7ba067066
Dylan Ferguson <dylan.b.ferguson@gmail.com>
2019-01-23 22:09:40 +1100

    update bash script

---
+++ update.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+
+echo 'Updating Brew...'
+brew update
+brew upgrade
+brew cleanup -s
+brew doctor
+brew missing
+
+brew bundle dump --force
+if [[ $(git diff --stat Brewfile) != '' ]]; then
+  echo 'Updating Brewfile...'
+  git add Brewfile
+  git commit -m 'auto update Brewfile'
+  git push
+  echo 'Pushed'
+fi
+
+echo 'Up update...'
+mas outdated | terminal-notifier -message
+mas upgrade
+
+echo 'Updating NPM global packages...'
+npm update -g 
